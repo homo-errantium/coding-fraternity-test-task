506f585b6d17d2c128719e7fc581cbd93f4606c5
homo-errantium <homo.errantium@gmail.com>
2024-05-16 02:57:29 +0300

    add form

---
+++ practice/scripts/index.js
@@ -0,0 +1,55 @@
+const mainButton = document.querySelector('.main__button');
+const popupAdd = document.querySelector('.popup_type_add');
+
+/*--------ф-я открытия/закрытия попап-а------*/
+function closePopup(popup) {
+    document.removeEventListener("keydown", handleEscapeButton);
+    popup.classList.remove("popup_opened");
+}
+
+function openPopup(popup) {
+    document.addEventListener('keydown', handleEscapeButton);
+  popup.classList.add('popup_opened');
+}
+
+/*--------ф-я закрытия при клике на оверлей------*/
+function closePopupOverlay() {
+    const popupList = Array.from(document.querySelectorAll(".popup"));
+    popupList.forEach((popupElement) => {
+        popupElement.addEventListener("mousedown", (evt) => {
+            if (evt.target.classList.contains("popup_opened")) {
+                closePopup(popupElement);
+            }
+            if (evt.target.classList.contains("popup__close")) {
+                closePopup(popupElement);
+            }
+        });
+    });
+}
+
+closePopupOverlay();
+
+mainButton.addEventListener('click', () => {
+    openPopup(popupAdd);
+});
+
+/*--------ф-я закрытия по клавише------*/
+function handleEscapeButton(event) {
+  if (event.key === "Escape") {
+    const currentPopupItem = document.querySelector(".popup_opened");
+    closePopup(currentPopupItem);
+  }
+}
+
+/*--------добавление новых данных-----------*/
+function handleFormSubmitAdd(evt) {
+    evt.preventDefault();
+    addCard({ name: nameInputAdd.value, link: linkInputAdd.value });
+    closePopup(popupAdd);
+    evt.target.reset();
+    const buttonAdd = evt.submitter;
+    disableSubmitButton(buttonAdd, selectors);
+}
+
+/*--------вызов навешивания валидации-----------*/
+enableValidation(selectors);
